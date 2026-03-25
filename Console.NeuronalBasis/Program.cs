f3f1db2fb9d3135bb2ece750837a1b4a93c189a6
Gerhard Ahrens <Gerhard.Ahrens@lifeprojects.de>
2026-03-25 07:39:23 +0100

    Projektdateien hinzufügen.

---
+++ Console.NeuronalBasis/Program.cs
@@ -0,0 +1,168 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Lifeprojects.de">
+//     Class: Program
+//     Copyright © Lifeprojects.de 2026
+// </copyright>
+// <Template>
+// 	Version 3.0.2026.1, 08.1.2026
+// </Template>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>developer@lifeprojects.de</email>
+// <date>03.03.2026 14:26:39</date>
+//
+// <summary>
+// Basis Implementierung zu einem Neuronalen Netz.
+// Erzeugen eines Neurons
+// </summary>
+//-----------------------------------------------------------------------
+
+namespace Console.NeuronalBasis
+{
+    /* Imports from NET Framework */
+    using System;
+
+    public class Program
+    {
+        public Program()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.CursorVisible = false;
+        }
+        private static void Main(string[] args)
+        {
+            CMenu unterMenu = new CMenu("Untermenü");
+            unterMenu.AddItem("Untermenüpunkt 1", () => UnterMenuPoint("A"), "🖥");
+            unterMenu.AddItem("Untermenüpunkt 2", () => UnterMenuPoint("B"), "🔊");
+
+            CMenu mainMenu = new CMenu("Implementierung eines Neuronalen Netzes");
+            mainMenu.AddItem("Ein Neuron erzeugen", MenuPoint1);
+            //mainMenu.AddSubMenu("Einstellungen", unterMenu, "⚙");
+            mainMenu.AddItem("Beenden", () => ApplicationExit());
+            mainMenu.Show();
+        }
+
+        private static void ApplicationExit()
+        {
+            Environment.Exit(0);
+        }
+
+        private static void MenuPoint1()
+        {
+            Console.Clear();
+
+            NeuralNetwork net = new NeuralNetwork(inputSize: 2, layerSizes: new int[] { 3, 1 } );
+
+            double[] input = { 0.5, 0.8 };
+
+            double[] result = net.Predict(input);
+
+            Console.WriteLine(result[0]);
+
+            CMenu.Wait();
+        }
+
+        private static void UnterMenuPoint(string param)
+        {
+            Console.Clear();
+
+            CMenu.Wait(param);
+        }
+    }
+
+    #region Implementierung Neuron
+    internal class Neuron
+    {
+        public double[] Weights;
+        public double Bias;
+
+        public Neuron(int inputCount)
+        {
+            Random rand = new Random();
+
+            this.Weights = new double[inputCount];
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                this.Weights[i] = rand.NextDouble() * 2 - 1;
+            }
+
+            this.Bias = rand.NextDouble() * 2 - 1;
+        }
+
+        public double Activate(double[] inputs)
+        {
+            double sum = this.Bias;
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                sum += inputs[i] * this.Weights[i];
+            }
+
+            return Sigmoid(sum);
+        }
+
+        private double Sigmoid(double x)
+        {
+            return 1.0 / (1.0 + Math.Exp(-x));
+        }
+    }
+
+    internal class Layer
+    {
+        public Neuron[] Neurons;
+
+        public Layer(int neuronCount, int inputCount)
+        {
+            this.Neurons = new Neuron[neuronCount];
+
+            for (int i = 0; i < neuronCount; i++)
+            {
+                this.Neurons[i] = new Neuron(inputCount);
+            }
+        }
+
+        public double[] Forward(double[] inputs)
+        {
+            double[] outputs = new double[this.Neurons.Length];
+
+            for (int i = 0; i < this.Neurons.Length; i++)
+            {
+                outputs[i] = this.Neurons[i].Activate(inputs);
+            }
+
+            return outputs;
+        }
+    }
+
+    internal class NeuralNetwork
+    {
+        private Layer[] Layers;
+
+        public NeuralNetwork(int inputSize, int[] layerSizes)
+        {
+            this.Layers = new Layer[layerSizes.Length];
+
+            int previousSize = inputSize;
+
+            for (int i = 0; i < layerSizes.Length; i++)
+            {
+                this.Layers[i] = new Layer(layerSizes[i], previousSize);
+                previousSize = layerSizes[i];
+            }
+        }
+
+        public double[] Predict(double[] inputs)
+        {
+            double[] output = inputs;
+
+            foreach (var layer in this.Layers)
+            {
+                output = layer.Forward(output);
+            }
+
+            return output;
+        }
+    }
+    #endregion Implementierung Neuron
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+﻿# Implementierung eines Neuronalen Netzes
+
+![NET](https://img.shields.io/badge/NET-10.0-green.svg)
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
+![VS2026](https://img.shields.io/badge/Visual%20Studio-2026-white.svg)
+![Version](https://img.shields.io/badge/Version-1.0.2026.0-yellow.svg)]
+
+## Basis Implementierung zu einem Neuronalen Netz
+Das kleine Konsolenprogramm dient der Implementierung und Test eines einfachen Neuronalen Netzwerkes. Das Projekt dient der Migration alter Arbeiten auf Basis von LISP und Fortran zum Thema der Neuronalen Netzte. Nach und nach will ich versuchen auf der Grundidee eines Neurons das Beispiel zu erweitern.
+
+### Neuron
+Ein künstliches Neuron berechnen:\
+<img src="Neuron_A.png" style="width:350px;"/>
+
+Mögliche Aktivierungsfunktion: **Sigmoid**\
+<img src="Neuron_B.png" style="width:250px;"/>
+
+Das Ergebnis ist nach **Sigmoid** ein Wert zwischen 0 und 1.
+```text
+0.02 → sehr unwahrscheinlich
+0.50 → unklar
+0.95 → sehr wahrscheinlich
+```
+Hinweis: da es sich nur um eine Beispielimplementierung handelt, ist das Netz nicht trainiert. Die verwendete Gewicht sind daher *Random*. Deshalb sind die Ergebnisse zufällig.
+
+## Beispielsource
+
+```csharp
+NeuralNetwork net = new NeuralNetwork(inputSize: 2, layerSizes: new int[] { 3, 1 } );
+double[] input = { 0.5, 0.8 };
+double[] result = net.Predict(input);
+```
+
+```xml
+```
+
+```json
+```
